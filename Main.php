2788d1d2397b6089a066930148c4a727304a9550
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-09-16 19:35:24 -0300

    Add files via upload

--- Main.php
+++ Main.php
@@ -1,16 +1,20 @@
-<?php
-require_once "/vendor/autoload.php";
-?>
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="Main.css">
+        <link rel="icon" type="imagem/png" href="/img/famicon.png" />
+        <title>Tradutor Web</title>
     </head>
     <body>
         <main>
-        <form action="text.php" method=get target="input">
-            <input type=text name=nome id="name" placeholder="Insira aqui a url"><br>
-            <input type=submit id="but">
-        </form>
+            <h1>Tradutor</h1>
+            <h2>Traduzindo páginas em Inglês para o português</h2> <p align=center><img src="/img/famicon.png" height=100px width=100px></p>
+            <div id="form-items" align=center>
+                <form action="src/text.php" method=get target="input">
+                    <input type=text name="url" id="url" placeholder="   Insira aqui a url"><br>
+                    <input type=submit id="but">
+                </form>
+            </div>
         </main>
     </body>
 </html>
