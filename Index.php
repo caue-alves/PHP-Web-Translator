bdcab6e821c6cbba5e1f3494737e12eb1efc8f4c
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-09-17 09:16:34 -0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -4,13 +4,13 @@
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="Main.css">
-        <link rel="icon" type="imagem/png" href="/img/famicon.png" />
+        <link rel="icon" type="imagem/png" href="https://github.com/caue-alves/PHP-Web-Translator/blob/master/img/famicon.png?raw=true" />
         <title>Tradutor Web</title>
     </head>
     <body>
         <main>
             <h1>Tradutor</h1>
-            <h2>Traduzindo páginas em Inglês para o português</h2> <p align=center><img src="/img/famicon.png" height=100px width=100px></p>
+            <h2>Traduzindo páginas em Inglês para o português</h2> <p align=center><img src="https://github.com/caue-alves/PHP-Web-Translator/blob/master/img/famicon.png?raw=true" height=100px width=100px></p>
             <div id="form-items" align=center>
                 <form action="src/text.php" method=get target="input">
                     <input type=text name="url" id="url" placeholder="   Insira aqui a url"><br>
