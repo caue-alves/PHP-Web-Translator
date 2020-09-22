4b34779e1ed335e33e2be11fd5fb91169add7759
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-09-22 16:11:40 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="assets/css/Main.css">
+        <link rel="icon" type="/imagem/png" href="https://github.com/caue-alves/PHP-Web-Translator/blob/master/img/famicon.png?raw=true" />
+        <title>Tradutor Web</title>
+    </head>
+    <body>
+        <main>
+            <h1>Tradutor</h1>
+            <h2>Traduzindo páginas em Inglês para o português</h2> <p align=center><img src="https://github.com/caue-alves/PHP-Web-Translator/blob/master/img/famicon.png?raw=true" height=100px width=100px padding=0.5em></p>
+            <div id="form-items" align=center>
+                <form action="text.php" method=get target="input">
+                    <input type=text name="url" id="url" placeholder="   Insira aqui a url"><br>
+                    <input type=submit id="but">
+                </form>
+            </div>
+        </main>
+    </body>
+</html>
